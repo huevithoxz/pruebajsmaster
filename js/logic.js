2d6eed8f72f8d8738369b3b0460e1a16d48238e9
huevithoxz <75405512+huevithoxz@users.noreply.github.com>
2022-01-13 01:58:13 -0300

    algunos ajustes

--- js/logic.js
+++ js/logic.js
@@ -1,8 +1,8 @@
-const ROCK = 0;
+const ROCK = "rock";
 
-const PAPER = 1;
+const PAPER = "paper";
 
-const SCISSORS = 2;
+const SCISSORS = "scissors";
 
 const WIN = 0;
 
@@ -10,9 +10,17 @@ const TIE = 1;
 
 const LOST = 2;
 
+var points = 0; 
+var rounds = 0; 
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
+const resultText = document.getElementById("start-text");
+const userImg = document.getElementById("user-img");
+const machineImg = document.getElementById("machine-img");
+
+const ranked = document.getElementById("reanked");
+const round = document.getElementById("round");
 
 rockBtn.addEventListener("click", () => {
   play(ROCK);
@@ -24,26 +32,60 @@ scissorsBtn.addEventListener("click", () => {
   play(SCISSORS);
 });
 
-function play(userOption)
-{
-    const machineOption = Math.floor(Math.random() * 3);
-    const result = calcResult(userOption, machineOption);
-
-    switch(result)
-    {
-        case TIE : 
-        alert(machineOption+"EMPATE")
-        break;
-        case WIN : 
-        alert(machineOption+"GANASTE")
-        break;
-        case LOST : 
-        alert(machineOption+"PERDISTE")
-        break;
-    }
+function calMachineOption() {
+  const number = Math.floor(Math.random() * 3);
+  switch (number) {
+    case 0:
+      return ROCK;
+    case 1:
+      return PAPER;
+    case 2:
+      return SCISSORS;
+  }
 }
-function calcResult(userOption,machineOption) {
 
+function play(userOption) {
+
+
+  
+ 
+  userImg.src = "style/img/" + userOption + ".svg";
+  resultText.innerHTML = "Escogiendo";
+  const interval = setInterval(function () {const machineOption = calMachineOption();
+    machineImg.src = "style/img/" + machineOption + ".svg";}, 100);
+  setTimeout(function () {
+    clearInterval(interval)
+    const machineOption = calMachineOption();
+    const result = calcResult(userOption, machineOption);
+    
+    machineImg.src = "style/img/" + machineOption + ".svg";
+
+    switch (result) {
+      
+      case TIE:
+        resultText.innerHTML = "Has Empatado !";
+
+        break;
+      case WIN:
+       
+        resultText.innerHTML = "GANASTE";
+        console.log(points)
+        suma = points + 100
+        document.getElementById("ranked").innerHTML = "Puntaje: "  + suma;
+        
+        break;
+      case LOST:
+     
+        resultText.innerHTML = "PERDISTE";
+        resta = points - 30
+        document.getElementById("ranked").innerHTML = "Puntaje: "  + resta;
+        break;
+    } 
+   
+    updateDisplay(++rounds);
+  }, 1000);
+}
+function calcResult(userOption, machineOption) {
   if (userOption === machineOption) {
     return TIE;
   } else if (userOption === ROCK) {
@@ -56,4 +98,17 @@ function calcResult(userOption,machineOption) {
     if (machineOption === ROCK) return LOST;
     if (machineOption === PAPER) return WIN;
   }
+}
+
+
+function resetCounter() {
+    counterVal = 0;
+    updateDisplay(counterVal);
+}
+
+function updateDisplay(val) {
+    document.getElementById("round").innerHTML = "Ronda: "+val;
+}
+function updatePoints(val) {
+  document.getElementById("ranked").innerHTML = "Puntaje: "  +val;
 }
--- js/login.js
+++ js/login.js
@@ -1,1 +1,18 @@
+function login()
+{
+    var user , pass; 
 
+    user = document.getElementById("user").value;
+    pass = document.getElementById("pass").value;
+
+    if(user=== "matias" && pass === "1234")
+    {
+        document.getElementById("game").style.display = "flex"
+        document.getElementById("login-box").style.display = "none"
+    }
+    if (user !== "matias" && pass !== "1234" )
+    {
+        alert("Ingrese usuario y contraseña valida")
+    }
+
+}
